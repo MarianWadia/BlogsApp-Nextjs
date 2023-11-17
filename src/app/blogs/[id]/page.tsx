fa7f97b592d240ed0403d444f61bffd9630b1fa4
Marian Israel <marianwadia55@gmail.com>
2023-11-17 20:33:43 +0200

    finished blogDetails page

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -3,19 +3,21 @@ import { blogs } from '@/data/posts'
 import { useParams } from 'next/navigation'
 import React from 'react'
 import Comments from "@/app/components/Comments"
+import FormComment from '@/app/components/FormComment'
 
 const BlogDetails = () => {
     const {id} = useParams()
-    const clickedBlog = blogs.filter((blog) => blog.id === Number(id))
-    console.log(clickedBlog);
+    const filteredBlog = blogs.filter((blog) => blog.id === Number(id))
+    const singleBlog = filteredBlog[0]
     
   return (
-    <div className='max-w-4xl bg-white text-black mx-auto my-12 p-4  rounded-md flex justify-center flex-col gap-2 shadow-lg shadow-gray-300'>
-        <h2 className='text-center font-bold text-md'>{clickedBlog[0].title}</h2>
-        <p className='text-sm'>{clickedBlog[0].body}</p>
-        <h6 className='text-slate-500 text-xs italic text-end'>{clickedBlog[0].username}</h6>
+    <div className='max-w-3xl md:max-w-4xl bg-white text-black mx-4 md:mx-auto my-12 p-4 rounded-md flex justify-center flex-col gap-2 shadow-lg shadow-gray-300'>
+        <h2 className='text-center font-bold text-md'>{singleBlog.title}</h2>
+        <p className='text-sm'>{singleBlog.body}</p>
+        <h6 className='text-slate-500 text-xs italic text-end'>{singleBlog.username}</h6>
 
-        <Comments clickedBlog = {clickedBlog[0]} />
+        <Comments singleBlog = {singleBlog} />
+        <FormComment singleBlog = {singleBlog} />
     </div>
   )
 }
